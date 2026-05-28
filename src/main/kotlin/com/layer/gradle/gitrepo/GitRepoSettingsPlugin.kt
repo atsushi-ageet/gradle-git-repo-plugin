f667ac27d2f9ac435ce8818e29d64e958b4cc9a3
atsushi <atsushi@ageet.com>
2026-05-28 13:17:51 +0900

    Fix local cache

--- src/main/kotlin/com/layer/gradle/gitrepo/GitRepoSettingsPlugin.kt
+++ src/main/kotlin/com/layer/gradle/gitrepo/GitRepoSettingsPlugin.kt
@@ -7,6 +7,18 @@ import org.gradle.api.plugins.ExtensionAware
 class GitRepoSettingsPlugin : Plugin<Settings> {
 
     override fun apply(settings: Settings) {
+        settings.gradle.projectsLoaded { gradle ->
+            val rootProject = gradle.rootProject
+            if (rootProject.tasks.findByName("cleanLocalRepoCache") == null) {
+                val cleanCache = rootProject.tasks.register("cleanLocalRepoCache") { t ->
+                    t.doFirst { GitRepoPlugin.localReposCache.clear() }
+                }
+                rootProject.afterEvaluate {
+                    rootProject.tasks.maybeCreate("clean").dependsOn(cleanCache)
+                }
+            }
+        }
+
         val repos = settings.dependencyResolutionManagement.repositories
         val ext = GitRepoExtension(SettingsGitRepoContext(settings), repos)
         (repos as ExtensionAware).extensions.add("gitRepo", ext)
